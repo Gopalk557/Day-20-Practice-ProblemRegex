607e949c8f989a11fd6f306d62faf76134f013f9
GOPAL THAKUR <gopalk557@gmail.com>
2022-03-09 01:45:49 +0530

    [Gopal]Add:Email

--- Day 20 Practice ProblemRegex/RegularExp.cs
+++ Day 20 Practice ProblemRegex/RegularExp.cs
@@ -9,8 +9,8 @@ namespace Day_20_Practice_ProblemRegex
 {
      class RegularExp
     { 
-        public static string regex = "^[A-Z][a-z]{5}$";
-        public bool validateLastName(string sample)
+        public static string regex = "^[a-zA-Z0-9]{3,10}[.]?[a-zA-Z0-9]{3,10}[@][a-z]{3,10}[.][a-z]{2,3}[.]?([a-z]{2,2})?$";
+        public bool Email(string sample)
         {
             return Regex.IsMatch(sample, regex);
         }
